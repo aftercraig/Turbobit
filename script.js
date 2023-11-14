ace0d864c5f1e5101f9f5a9cd12b6d06cdc1a0cd
aftercraig <pavelboritko@gmail.com>
2023-11-14 23:44:53 +0500

    this is so...

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+require("dotenv").config
+const multer = require("multer")
+
 const express = require("express")
 const app = express()
+app.use(express.static(__dirname));
+
+const upload = multer({ dest: "Uploaded_files" })
 
 app.set("view engine", "ejs")
 
+/*
+app.get("/views/homepage.ejs", (req, res) => {
+    res.sender("/views/homepage.ejs")
+})
+*/
+
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("homepage")
 })
 
-app.listen(4000)
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.send("hi")
+})
+
+
+
+app.listen(8000)
